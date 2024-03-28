aa574a1b150127029dc0d2ad31624c5a58e63357
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-03-28 11:32:07 -0400

    Add files via upload

--- bash.sh
+++ bash.sh
@@ -1,23 +1,24 @@
 # This is print
-echo "Hello World";
-
+#echo "Hello World";
 # This divides code below divides 80 by 2
-
-n1=80;
-n2=2;
-
-let quotient=$n1/$n2;
-
-echo $quotient;
-
-operation2=$((n1/n2));
-
-echo $operation2;
-
+#n1=80;
+#n2=2;
+#let quotient=$n1/$n2;
+#echo $quotient;
+#operation2=$((n1/n2));
+#echo $operation2;
 #echo "Enter your name: "
-
 #read name
+#read -p "Enter your name: " name
+#echo "Welcome" $name "to shell scripting"
+#read -p "Please enter a number: " num
+#echo "You entered: "$num
+#total=$(($num+1))
+#let total=$num+1
+#echo "The sum is: "$total
 
-read -p "Enter your name: " name
+read -p "How old are you? " age
+let calcDays=$((age*365))
 
-echo "Welcome" $name "to shell scripting"
+echo $calcDays
+
