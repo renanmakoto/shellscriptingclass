aa4f2c06c8ac44da7e6c95e2c39cfd32ee8fd7d3
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-03-28 11:05:27 -0400

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,23 @@
+# This is print
+echo "Hello World";
+
+# This divides code below divides 80 by 2
+
+n1=80;
+n2=2;
+
+let quotient=$n1/$n2;
+
+echo $quotient;
+
+operation2=$((n1/n2));
+
+echo $operation2;
+
+#echo "Enter your name: "
+
+#read name
+
+read -p "Enter your name: " name
+
+echo "Welcome" $name "to shell scripting"
