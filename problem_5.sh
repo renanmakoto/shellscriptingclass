2ff39adbc37bd4dd172dc1984fc7b0b9639decc3
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2024-04-04 11:46:21 -0400

    Add files via upload

--- problem_5.sh
+++ problem_5.sh
@@ -1,18 +1,22 @@
 read -p "Favourite colour? 1-blue 2-yellow 3-red 4-orange (Press Ctrl + C to exit the program) : "  FAVCOLOUR
 if [ $FAVCOLOUR -eq 1 ]
 then
-	echo "blue"
+	echo "You chose blue"
 elif [ $FAVCOLOUR -eq 2 ]
 then 
-	echo "yellow"
+	echo "You chose yellow"
 elif [ $FAVCOLOUR -eq 3 ]
 then	
-	echo "red"
+	echo "You chose red"
+	echo "Soyuz nerushimy respublik svobodnik"
 elif [ $FAVCOLOUR -eq 4 ]
 then
 	echo "orange"
+else
+	echo "You must choose at least one. Run the program again."
 fi
 echo "End of the script"
 
 #Use "==" for strings in conditionals
+# e.g. "$FAVCOLOUR == 'red'"
 
